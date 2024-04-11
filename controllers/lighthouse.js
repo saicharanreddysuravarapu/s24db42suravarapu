4a2a35ff0e52e0ca21dfedb4a418e85746c44698
saicharanreddysuravarapu <charanreddysuravarapu@gmail.com>
2024-04-10 22:02:41 -0500

    Added part 4,5

--- controllers/lighthouse.js
+++ controllers/lighthouse.js
@@ -105,8 +105,32 @@ exports.lighthouse_delete = async function(req, res) {
     res.send(`{"error": Error deleting ${err}}`);
     }
     };
-    
-    
+// Handle a show one view with id specified by query
+exports.lighthouse_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await lighthouse.findById( req.query.id)
+    res.render('lighthousedetail',
+   { title: 'Lighthouse Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.lighthouse_create_Page = function(req, res) {
+ console.log("create view")
+ try{
+ res.render('lighthousecreate', { title: 'lighthouse Create'});
+ }
+ catch(err){
+ res.status(500)
+ res.send(`{'error': '${err}'}`);
+ }
+};
     
 
     
--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -1,16 +1,18 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('lighthouse', { title: 'Search Results - Light House' });
-// });
-
-// module.exports = router;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('lighthouse', { title: 'Search Results - Light House' });
+});
 
 var express = require('express');
 const lighthouse_controlers= require('../controllers/lighthouse');
 var router = express.Router();
 /* GET costumes */
 router.get('/', lighthouse_controlers.lighthouse_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', lighthouse_controlers.lighthouse_view_one_Page);
+/* GET create costume page */
+router.get('/create', lighthouse_controlers.lighthouse_create_Page);
 module.exports = router;
