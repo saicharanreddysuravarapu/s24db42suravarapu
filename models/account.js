34fa92cf5c57281a8a1433b24dec595a5320b11e
saicharanreddysuravarapu <charanreddysuravarapu@gmail.com>
2024-04-12 11:57:48 -0500

    Lab-13

--- models/account.js
+++ models/account.js
@@ -5,7 +5,8 @@ const accountSchema = new Schema({
  username: String,
  password: String
 });
+
 accountSchema.plugin(passportLocalMongoose);
-// We export the Schema to avoid attaching the model to the
+// We export the Schema to avoid attaching the model to the`
 // default mongoose connection.
 module.exports = mongoose.model("Account", accountSchema);
