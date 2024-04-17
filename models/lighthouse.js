6d243356a5f1877e9d21860350e730a663053f7a
saicharanreddysuravarapu <charanreddysuravarapu@gmail.com>
2024-04-17 18:24:03 -0500

    Bonus

--- models/lighthouse.js
+++ models/lighthouse.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const lighthouseSchema = new mongoose.Schema({
-  lighthouse_type: String,
-  size: String,
-  cost: Number
+  lighthouse_type: {
+    type: String,
+    required: [true, "Light House type is required"],
+    minlength: [1, "Type must be at least 1 character"],
+    maxlength: [50, "Type must be at most 50 characters"]
+  },
+  size: {
+    type: String,
+    required: [true, "Light House Size is required"],
+    minlength: [1, "Size must be at least 1"]
+  },
+  cost: {
+    type: Number,
+    required: [true, "Light House Cost is required"],
+    min: [1, "Cost must be at least 1"]
+  }
 });
 
-module.exports = mongoose.model("lighthouse", lighthouseSchema);
+module.exports = mongoose.model("Lighthouse", lighthouseSchema);
--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -25,12 +25,9 @@ router.get('/', lighthouse_controlers.lighthouse_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', lighthouse_controlers.lighthouse_view_one_Page);
 /* GET create costume page */
-router.get('/create', lighthouse_controlers.lighthouse_create_Page);
-/* GET create update page */
-// router.get('/update', lighthouse_controlers.lighthouse_update_Page);
+router.get('/create',secured, lighthouse_controlers.lighthouse_create_Page);
 /* GET delete costume page */
-router.get('/delete', lighthouse_controlers.lighthouse_delete_Page);
-
+router.get('/delete',secured, lighthouse_controlers.lighthouse_delete_Page);
 /* GET update costume page */
 router.get('/update', secured, lighthouse_controlers.lighthouse_update_Page);
 module.exports = router;
